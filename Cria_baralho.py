45bb50e8077d08d3da0eb5b95717e09fcace2026
Joao Pedro Santos <maxxjotinha@gmail.com>
2021-04-26 16:13:15 -0300

    Função que cria o baralho do jogo.

---
+++ Cria_baralho.py
@@ -0,0 +1,22 @@
+﻿def cria_baralho():
+    baralho = []
+    i = 2 
+    while i < 11 :
+        baralho.append('{0}♥'.format(i))
+        baralho.append('{0}♠'.format(i))
+        baralho.append('{0}♦'.format(i))
+        baralho.append('{0}♣'.format(i))
+        i += 1
+    
+    letras = ['A', 'J', 'K', 'Q']    
+
+    for i in letras:
+        baralho.append('{0}♣'.format(i))
+        baralho.append('{0}♦'.format(i))
+        baralho.append('{0}♠'.format(i))
+        baralho.append('{0}♥'.format(i))
+         
+
+    print(baralho)
+    print(len(baralho))
+    return baralho 
